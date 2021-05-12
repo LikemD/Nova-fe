99452afa00c202cdc2f5911bf3674542d6edf874
SwathySanthoshNeosoft <swathy.santosh@neosoftmail.com>
2021-05-12 18:33:33 +0530

    Revert "Revert "pages""
    This reverts commit 5b177d74f2f249eeaf243a2d6626584f4ee1eab0.

---
+++ pages/PaymentOptions.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="font-poppins button-green">
+    <!-- Title -->
+    <div class="flex justify-between">
+      <div class="text-2xl">Payment Type</div>
+
+      <div class="float-right">
+        <img src="../assets/icons/VectorClose.png" />
+      </div>
+    </div>
+    <!-- Title -->
+    <div class="line pb-4"></div>
+
+    <div>
+      <div class="flex">
+        <div>
+          <div class="text-xl">Cash</div>
+          <div class="bg-input-grey p-4 rounded-lg">
+            <div>
+              <img src="../assets/icons/Payment_One.png" />
+            </div>
+          </div>
+        </div>
+        <div>
+          <div class="text-xl">Mobile Money</div>
+          <div class="bg-input-grey p-4 rounded-lg">
+            <div class="flex">
+              <img src="../assets/icons/vodafone 1.png" />
+              <img src="../assets/icons/mtn 1.png" />
+              <img src="../assets/icons/airteltigo 1.png" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.line {
+  border-top: 1px solid #adabab;
+}
+img {
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ pages/alert.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+      <div class="flex justify-center h-screen items-center bg-gray-200 antialiased bg-opacity-90">
+      <div class="flex flex-col w-11/12 sm:w-5/6 lg:w-1/2 max-w-2xl mx-auto rounded-lg border border-gray-300 shadow-xl">
+        <div
+          class="flex flex-row justify-between p-6 bg-white border-b border-gray-200 rounded-tl-lg rounded-tr-lg"
+        >hello
+        </div>
+      </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
