8352e6e125e4061d41bf640a9c61bbf54493a8b1
SwathySanthoshNeosoft <swathy.santosh@neosoftmail.com>
2021-05-12 20:19:09 +0530

    initial commit-pages

---
+++ pages/CylinderScannedSuccessfully.vue
@@ -0,0 +1,45 @@
+<template>
+  <!-- Cylinder Scanned Successfully -->
+  <div class="font-poppins p-6">
+    <div>
+      <div class="m-0 m-auto w-20">
+        <div class="bg-green rounded-full tickBorder">
+          <div class="p-4">
+            <img src="../assets/icons/tick.png" />
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="text-button-green text-3xl flex justify-center pt-5">
+      Cylinder Scanned Successfully
+    </div>
+    <div class="flex justify-between pt-10">
+      <div>
+        <button
+          type="reset"
+          class="rounded pl-6 pr-7 pt-2 pb-2 text-white items-center focus:outline-none bg-button-color-one"
+        >
+          Scan Another Cylinder
+        </button>
+      </div>
+      <div>
+        <button
+          type="reset"
+          class="rounded pl-12 pr-12 pt-2 pb-2 text-white items-center bg-button-green focus:outline-none"
+        >
+          Complete Scan
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.tickBorder {
+  border: 7px solid #f3f3f3;
+}
+</style>
---
+++ pages/ModelOfReceipt.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="font-poppins font-bold text-button-green">
+    <div class="p-3">
+      <div class="text-2xl pb-2">
+        <p>Mode of Receipt</p>
+      </div>
+      <div class="line pb-4"></div>
+      <div
+        class="flex space-x-4 border border-border rounded-md pt-6 pb-6 pl-3 pr-3"
+      >
+        <div class="leading-10">
+          <input type="radio" class="bg-border" />
+        </div>
+        <div class="w-1/2">
+          <img src="../assets/icons/delivery.png" />
+        </div>
+        <div class="leading-10 text-xl font-bold">
+          <label for="">Delivery</label>
+        </div>
+      </div>
+      <div class="pt-6">
+        <div class="flex space-x-8 border border-border rounded-md p-3">
+          <div class="pt-6">
+            <input type="radio" class="bg-border" />
+          </div>
+          <div class="w-1/4">
+            <img src="../assets/icons/pickup.png" />
+          </div>
+          <div class="pt-6 text-xl font-bold">
+            <label for="">Self-Pickup</label>
+          </div>
+        </div>
+      </div>
+
+      <div class="pt-6">
+        <div class="flex space-x-8 border border-border rounded-md p-3">
+          <div class="pt-6">
+            <input type="radio" class="bg-border" />
+          </div>
+          <div class="w-1/4">
+            <img src="../assets/icons/pickup.png" />
+          </div>
+          <div class="pt-6 text-xl font-bold">
+            <label for="">Assign-Pickup</label>
+          </div>
+        </div>
+      </div>
+      <div class="pt-6 flex justify-center">
+        <button
+          class="bg-button-green text-white focus:outline-none pt-2 pb-2 pr-6 rounded-md pl-6"
+        >
+          Confirm
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.line {
+  border-top: 1px solid #adabab;
+}
+</style>
